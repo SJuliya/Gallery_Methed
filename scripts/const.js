f40686575c85cd5745c1b332d870b264d8abb9ac
Juliya Sartasova <anjee2007@mail.ru>
2022-07-09 16:50:48 +0300

    день 3 доделала

---
+++ scripts/const.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.unsplash.com';
+export const API_URL_PHOTOS = `${API_URL}/photos`;
+export const ACCESS_KEY = '4HVwCjWoau8W51R2TOkyTDGSZ7tPVoEI-aLCb_vury0';
+export const SECRET_KEY = 'z_h6khkhRLBHQ1bhRO5BwYu1aVXCHX_38soC4EY9RKc';
--- scripts/renderPhoto.js
+++ scripts/renderPhoto.js
@@ -3,7 +3,7 @@ import {createElem} from "./createElem.js";
 export const renderPhoto = (photoWrapper, photo) => {
     console.log(photo);
     const img = createElem('img', {
-        class: 'photo__picture',
+        className: 'photo__picture',
         src: photo.urls.regular,
         alt: photo.description || photo.alt_description,
         style: 'max-height: 80vh;',
@@ -34,6 +34,10 @@ export const renderPhoto = (photoWrapper, photo) => {
         textContent: photo.likes,
     });
 
+    if(!photoLike.likedByUser) {
+        photoLike.classList.add('photo__like_o');
+    }
+
     const photoDownload = createElem('a', {
         className: 'photo__download',
         download: 'true',
@@ -45,25 +49,3 @@ export const renderPhoto = (photoWrapper, photo) => {
     photoControl.append(photoLike, photoDownload);
     photoWrapper.append(img, author, photoControl);
 };
-
-/*         <img class="photo__picture"
-             src="https://images.unsplash.com/photo-1654363137357-9d897b5a20d7?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMDE0MzF8MHwxfGFsbHx8fHx8fHx8fDE2NTQ1MjMzNjE&amp;ixlib=rb-1.2.1&amp;q=80"
-             alt="null"
-             style="max-height: 80vh;">
-        <a class="photo__author"
-          href="https://unsplash.com/@supergios">
-          <img src="https://images.unsplash.com/profile-1600184424687-de96bd61fa67image?ixlib=rb-1.2.1&amp;crop=faces&amp;fit=crop&amp;w=64&amp;h=64"
-            alt="Always looking for the next adventure to capture the next shot. Seize the day. Sony A7 IV . There are more images behind my Unplash, for commercial projects please contact me.  John 3:16"
-            title="Jonny Gios">
-          <span>Jonny Gios</span>
-        </a>
-        <div class="photo__control">
-          <button id="JIqH1ps4eK8" class="photo__like">30</button>
-          <a class="photo__download"
-             download="true"
-             href="https://unsplash.com/photos/JIqH1ps4eK8/download?ixid=MnwzMDE0MzF8MHwxfGFsbHx8fHx8fHx8fDE2NTQ1MjMzNjE"
-             target="_blank">
-          </a>
-        </div>
-
- */

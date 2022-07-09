e40387ca06ed706d254e0a95ad9cae82d7fa2115
Juliya Sartasova <anjee2007@mail.ru>
2022-07-09 15:50:41 +0300

    день 3

--- scripts/createCardPhoto.js
+++ scripts/createCardPhoto.js
@@ -26,21 +26,21 @@ export const createCardPhoto = async (data) => {
         href: `page.html?photo=${data.id}`,
     });
 
-    const photo = await loadImage(data.urls.small, data.alt_description);
-    cardItem.height
+    const photo = await loadImage(data.urls.small, data.description);
 
     const author = createElem('a', {
         className: 'card__author',
         href: data.user.links.html,
     });
 
-    const avatarAuthor = new Image();
-    avatarAuthor.className = 'author__photo';
-    avatarAuthor.src = data.user.profile_image.medium;
-    avatarAuthor.width = '32';
-    avatarAuthor.height = '32';
-    avatarAuthor.alt = data.user.bio;
-    avatarAuthor.title = data.user.username;
+    const avatarAuthor = createElem('img', {
+        className: 'author__photo',
+        src: data.user.profile_image.medium,
+        width: '32',
+        height: '32',
+        alt: data.user.bio,
+        title: data.user.username,
+    });
 
     author.append(avatarAuthor);
 
--- scripts/renderPhoto.js
+++ scripts/renderPhoto.js
@@ -1,6 +1,50 @@
+import {createElem} from "./createElem.js";
+
 export const renderPhoto = (photoWrapper, photo) => {
     console.log(photo);
-}
+    const img = createElem('img', {
+        class: 'photo__picture',
+        src: photo.urls.regular,
+        alt: photo.description || photo.alt_description,
+        style: 'max-height: 80vh;',
+    });
+
+    const author = createElem('a', {
+        className: 'photo__author',
+        href: photo.user.links.html,
+    });
+
+    const avatarAuthor = createElem('img', {
+        src: photo.user.profile_image.medium,
+        alt: photo.user.bio,
+        title: photo.user.username,
+    });
+
+    const userName = createElem('span', {
+        textContent: photo.user.username,
+    });
+
+    const photoControl = createElem('div', {
+        className: 'photo__control',
+    });
+
+    const photoLike = createElem('button', {
+        id: photo.id,
+        className: 'photo__like',
+        textContent: photo.likes,
+    });
+
+    const photoDownload = createElem('a', {
+        className: 'photo__download',
+        download: 'true',
+        href: photo.urls.full,
+        target: '_blank',
+    });
+
+    author.append(avatarAuthor, userName);
+    photoControl.append(photoLike, photoDownload);
+    photoWrapper.append(img, author, photoControl);
+};
 
 /*         <img class="photo__picture"
              src="https://images.unsplash.com/photo-1654363137357-9d897b5a20d7?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixid=MnwzMDE0MzF8MHwxfGFsbHx8fHx8fHx8fDE2NTQ1MjMzNjE&amp;ixlib=rb-1.2.1&amp;q=80"
